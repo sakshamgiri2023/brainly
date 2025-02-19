fa865bece95e350c19651e8dd1d9ae84cba8e831
sakshamgiri2023 <sakshamgiri500@gmail.com>
2025-02-20 01:23:17 +0530

    updated the Input.tsx file

--- brainly-frontend/src/components/Input.tsx
+++ brainly-frontend/src/components/Input.tsx
@@ -10,7 +10,7 @@ export function Input({placeholder, reference}: InputProps) {
         <div>
             {/* Input field with the provided placeholder and reference */}
             <input 
-                ref={reference} // Attaching the reference to the input field
+                ref={reference} // Attaching the reference to the input field  
                 placeholder={placeholder} // Setting the placeholder text for the input field
                 type={"text"} // Defining the input type as text
                 className="px-4 py-2 border rounded m-2" // Tailwind CSS classes for styling the input field
